6f2c8bd1ef21c224e56ec2b4a304ce353c3ba1b7
guifigueiroa <guilhermefigueiroa@MacBook-Air-de-Guilherme.local>
2014-06-14 18:00:58 -0300

    Adicionando permutação

---
+++ permutacao.m
@@ -0,0 +1,11 @@
+function perm = permutacao( bloco, rodada )
+
+% calcula o shift de acordo com a rodada
+shift = mod(rodada-1,8)+1
+
+% faz a permutação de acordo com o valor de shift
+perm(1:8-shift) = bloco(shift+1:8);
+perm(9-shift:8) = bloco(1:shift);
+
+end
+
