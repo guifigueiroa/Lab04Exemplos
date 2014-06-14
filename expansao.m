73b1ae87b936310c109c4cf68c4916226632c631
Barbara Barbosa <barbarab.claudino@gmail.com>
2014-06-14 17:45:24 -0300

    corrigido ponto e virgula

--- expansao.m
+++ expansao.m
@@ -1,7 +1,7 @@
 function exp = expansao(bloco)
 
 %pega o vetor de tam 8 e transforma em matriz 2x4
-original = [bloco(1:4); bloco(5:8)]
+original = [bloco(1:4); bloco(5:8)];
 %com a primeira coluna 
 first_column = original(:,1);
 first_column =  [first_column(end);first_column(1)];
