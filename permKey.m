78cc9c97a10893d2590e85165768f8d5f3b65e8c
Barbara Barbosa <barbarab.claudino@gmail.com>
2014-06-14 20:25:40 -0300

    Corrigido permKey pra sempre ter o msm tamanho de retorno

--- permKey.m
+++ permKey.m
@@ -1,5 +1,8 @@
 function new_key = permKey(key, round)
 	bin_round = dec2bin(round) - '0';
 	n_key = somaPoliMod(bin_round,key,2);
-	new_key = [n_key 0];
+	if (length(n_key) < 8)
+		tam = 8 - length(n_key);
+		new_key = [zeros(tam) n_key];
+	end
 end
