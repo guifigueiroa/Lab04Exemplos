3b38a2d2e90ee691e2e9b45121f198d145a2f920
guifigueiroa <guilhermefigueiroa@MacBook-Air-de-Guilherme.local>
2014-06-14 19:42:07 -0300

    tamanho da chave atualizado

--- mainExp4.m
+++ mainExp4.m
@@ -2,7 +2,7 @@ nBits = 16;
 nRodadas = 25;
 nBlocos = 10;
 
-K = rand(nBlocos,nBits)>.5; %obtem uma chave aleatoria
+K = rand(nBlocos,12)>.5; %obtem uma chave aleatoria
 
 P = rand(nBlocos,nBits)>.5; %obtem um bloco de bits aleatorio
 C = exp4(P,K,nRodadas);
