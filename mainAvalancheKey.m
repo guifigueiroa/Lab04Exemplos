a789bd41e6fe2ef5c27572afa929942ed0c7fcff
Barbara Barbosa <barbarab.claudino@gmail.com>
2014-06-15 10:14:34 -0300

    AvalancheKey modificado para o algoritmo maluco (e grafico)

--- mainAvalancheKey.m
+++ mainAvalancheKey.m
@@ -1,4 +1,4 @@
-nBits = 8;
+nBits = 16;
 nRepeticoes = 100;
 nRodadas = 25;
 
@@ -7,22 +7,22 @@ entropia = zeros(1,nRodadas);
 for rodadas=1:nRodadas
     matriz = zeros(nBits,nBits);
     for repeticoes=1:nRepeticoes;
-        for bit=1:nBits
+        for bit=1:(nBits/2)
             P = rand(1,nBits)>.5; %obtem uma chave aleatoria
 
-            K1 = rand(1,nBits)>.5; %obtem um bloco de bits aleatorio
-            C1 = redePerm(P,K1,rodadas);
+            K1 = rand(1,8)>.5; %obtem um bloco de bits aleatorio
+            C1 = exp4(P,K1,rodadas);
 
             K2 = K1;
             K2(bit) = ~K2(bit); %inverte um dos bits da chave P1
-            C2 = redePerm(P,K2,rodadas);
+            C2 = exp4(P,K2,rodadas);
 
             %computa o que mudou na cifra
             matriz(bit,:) = matriz(bit,:) + xor(C1,C2);
         end
     end
     disp (rodadas)
-    fflush(stdout);
+    %fflush(stdout);
     matriz = matriz/nRepeticoes;
     % Aqui, o max e' utilizado para garantir que log(0) = 0
     entropia(rodadas) = mean(mean((max(-log2(matriz).*matriz,0) + max(-log2(1-matriz).*(1-matriz),0))));
