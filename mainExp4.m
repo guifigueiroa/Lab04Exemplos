f10e05c8ab72351591847918fdf3b2308dc10298
guifigueiroa <guilhermefigueiroa@MacBook-Air-de-Guilherme.local>
2014-06-14 21:31:03 -0300

    substituição corrigida e chave alterada para 8bits

--- mainExp4.m
+++ mainExp4.m
@@ -2,7 +2,7 @@ nBits = 16;
 nRodadas = 25;
 nBlocos = 10;
 
-K = rand(1,12)>.5; %obtem uma chave aleatoria
+K = rand(1,8)>.5; %obtem uma chave aleatoria
 
 P = rand(nBlocos,nBits)>.5; %obtem um bloco de bits aleatorio
 C = exp4(P,K,nRodadas);
--- substituicao.m
+++ substituicao.m
@@ -3,4 +3,8 @@ function subs = substituicao( bloco )
     irredutivel = [1 0 0 0 1 1 1 0 1];
     [subs,] = euclidesEstendidoPolinomial(bloco,irredutivel,2);
 
+    if (length(subs) < 8)
+		tam = 8 - length(subs);
+		subs = [subs zeros([1,tam])];
+	end
 end
