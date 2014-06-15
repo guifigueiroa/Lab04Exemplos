15226a927076c941ce32a8da74f524e5c5b10ac7
Barbara Barbosa <barbarab.claudino@gmail.com>
2014-06-14 21:28:10 -0300

    voltando a 8 bits!

--- permKey.m
+++ permKey.m
@@ -1,8 +1,8 @@
 function new_key = permKey(key, round)
 	bin_round = dec2bin(round) - '0';
 	new_key = somaPoliMod(bin_round,key,2);
-	if (length(new_key) < 12)
-		tam = 12 - length(new_key);
+	if (length(new_key) < 8)
+		tam = 8 - length(new_key);
 		new_key = [zeros([1,tam]) new_key];
 	end
 end
