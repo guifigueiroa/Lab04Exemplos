e048af5446e67fa3e242964eb37225a8d6b290be
Barbara Barbosa <barbarab.claudino@gmail.com>
2014-06-14 21:13:00 -0300

    Corrigido permKey (de vdd)

--- permKey.m
+++ permKey.m
@@ -1,8 +1,8 @@
 function new_key = permKey(key, round)
 	bin_round = dec2bin(round) - '0';
-	n_key = somaPoliMod(bin_round,key,2);
-	if (length(n_key) < 8)
-		tam = 8 - length(n_key);
-		new_key = [zeros(tam) n_key];
+	new_key = somaPoliMod(bin_round,key,2);
+	if (length(new_key) < 12)
+		tam = 12 - length(new_key);
+		new_key = [zeros([1,tam]) new_key];
 	end
 end
